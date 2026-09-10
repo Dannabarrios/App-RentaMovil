f7bddca1602ebe3e406eb1dbd95bf9ce23dd7db8
vanessaperdomo <p.laurav2@gmail.com>
2026-09-10 01:46:38 -0500

    fix(ui): corregir estilo de superposicion de carga en WompiCheckoutModal

--- app-drivique/modules/reservation/components/WompiCheckoutModal.tsx
+++ app-drivique/modules/reservation/components/WompiCheckoutModal.tsx
@@ -211,7 +211,11 @@ const styles = StyleSheet.create({
     position: "relative",
   },
   loadingOverlay: {
-    ...StyleSheet.absoluteFillObject,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     alignItems: "center",
     justifyContent: "center",
     zIndex: 10,
